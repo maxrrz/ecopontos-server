e9f21b8c84bbb986f384a164b9a878cb1316b75d
Max <151743346+maxrrz@users.noreply.github.com>
2025-02-17 20:30:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 const app = express();
 app.use(express.json());
